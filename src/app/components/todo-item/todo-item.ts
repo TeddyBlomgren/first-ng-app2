ca04f11ae64b570a14a0ff50d8f1e0031fe4653f
Teddy . <Teddy_95@live.se>
2025-09-23 13:15:13 +0200

    todo-standalone

--- src/app/components/todo-item/todo-item.ts
+++ src/app/components/todo-item/todo-item.ts
@@ -3,18 +3,18 @@ import { Todo } from '../../models/todo.type';
 import { HighlightCompletedTodoDirective } from '../../directives/highlight-completed-todo';
 import { Output, EventEmitter } from '@angular/core';
 
-
 @Component({
   selector: 'app-todo-item',
   imports: [HighlightCompletedTodoDirective],
   templateUrl: './todo-item.html',
-  styleUrl: './todo-item.css'
+  standalone: true,
+  styleUrl: './todo-item.css',
 })
 export class TodoItemcomponent {
   todo = input.required<Todo>();
   @Output() todoToggled = new EventEmitter<Todo>();
 
-
-  todoClicked() { this.todoToggled.emit(this.todo()); 
+  todoClicked() {
+    this.todoToggled.emit(this.todo());
   }
 }
